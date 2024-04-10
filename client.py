0c12be7b3fba6a4273404e482f34029b8b603661
Reza-Tahmasbi <rezatb2002@gmail.com>
2024-04-10 17:14:46 +0330

    second commit

--- client.py
+++ client.py
@@ -1,16 +1,15 @@
 import threading
 import socket
 
-nickname = input("Choose a nickname: ")
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 15000))
-
+    
 def recieve():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
-            if message == "NICK":
-                client.send(nickname.encode('ascii'))
+            print(message)
+            if message == "mode":
+                user_input = input("ask: ")
+
             else:
                 print(message)
         except:
@@ -23,6 +22,23 @@ def write():
         message = f'{nickname}: {input("")}'
         client.send(message.encode("ascii"))
         
+        
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 15000))
+
+print("Welcome to your pleasent Chat Room")
+while True:
+    user_input = input("ask: ")
+    if user_input.split()[0] == "Registeration":
+        register(user_input)
+        break
+    elif user_input.split()[0] == "Login":
+        login(user_input)
+        break
+    else:
+        print(f"Command {user_input} is not a valid one!\nRegister: Registration <username><password>\nLogin: Login <username>")
+    
+    
 recieve_thread = threading.Thread(target = recieve)
 recieve_thread.start()
 
--- server.py
+++ server.py
@@ -1,9 +1,12 @@
 import threading
+import sqlite3
 import socket
 
 host = '127.0.0.1' # local
 port = 15000
 
+conn = sqlite3.connect('your_database.db')
+c = conn.cursor()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
 server.listen()
@@ -37,15 +40,78 @@ def handle(client):
 # when the client connects, ask for nickname
 # add the connected client to the chat
 # start a thread to handle the connection for this particular client
+def login(user_input, client):
+    username = input(user_input.split()[1])
+    password = input(user_input.split()[2])
+    if ' ' in username():
+        return "username must not include white space", False
+    if ' ' in password:
+        return "password must not include white space", False
+    results = c.execute("SELECT * FROM user WHERE username=? AND password=?", (username, password))
+    results = c.fetchall()
+    if results:
+        return "You have logged in successfuly!", True
+    else:
+        client.close()
+
+def register(user_input):
+    username = input(user_input.split()[1])
+    password = input(user_input.split()[2])
+    if ' ' in username():
+        return "username must not include white space", False
+    if ' ' in password:
+        return "password must not include white space", False
+    results = c.execute("SELECT * FROM user WHERE username=?", (username,))
+    results = c.fetchall()
+    if results:
+        return "username already exists!", False
+    else:
+        c.execute("INSERT INTO user (username, password) VALUES (?,?)", (username, password))
+        conn.commit()
+        return "You are successfully Registered!", True
+
+def generate_key(password):
+    C
+    
 def receive():
     while True:
+        first_time = True
         client, address = server.accept()
         print(f"Connected with {str(address)}")
-        
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
+
+
+        if first_time:
+            # Choose what to do?
+            server_mess = """
+                    Register: Registration <username><password>
+                    Login: Login <username>
+            """
+            client.send(server_mess.encode('ascii'))
+            
+            user_command = client.recv(1024).decode('ascii')
+            if user_command.split()[0] == "Registeration":
+                message, first_time = register()
+                client.send(message.encode('ascii'))
+            elif user_command.split()[0] == "Login":
+                message, first_time = login()
+                client.send(message.encode('ascii'))
+            else:
+                print(f"Command {user_command} is not a valid one!")
+                continue
+        else:
+            pass
+    
+        if nickname == 'admin':
+            client.send('PASS'.encode('ascii'))
+            password = client.recv(1024).decode('ascii')
+            
+            if password != 'adminpass':
+                client.send(('REFUSE'.encode('ascii')))
+                client.close()
+                continue
+            
         nicknames.append(nickname)
-        client.append(client)
+        clients.append(client)
         
         print(f"Nickname of client is {nickname}!")
         broadcast(f'{nickname} joined the chat!'.encode('ascii'))
